dbe6bbb9038811d151878f28d86c9c232798b532
Marie <marie-sarah@live.com>
2019-03-02 17:28:03 -0500

    Footer and slight CSS adjustments

--- wp-content/themes/forrrest/footer.php
+++ wp-content/themes/forrrest/footer.php
@@ -20,17 +20,22 @@
 				'container_class' => 'footer_menu',
 			) );
 			?>
+			<div class="author-info">
+			<span>
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'forrrest' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'forrrest' ), 'WordPress' );
 				?>
 			</a>
+			</span>
 			<span class="sep"> | 
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'forrrest' ), 'forrrest', '<a href="https://www.mariefelton.com">marie felton</a>' );
 				?>
+				</span>
+				</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 	</div><!-- .page-container -->
